942bf83e7c1ca226084cc2a528e6eec0ecbba4b2
Amey Sunu <ameysunu00719@gmail.com>
2020-10-24 13:53:09 +0300

    login added

--- lib/login.dart
+++ lib/login.dart
@@ -25,6 +25,60 @@ class _LoginState extends State<Login> {
                 decoration: BoxDecoration(
                     color: HexColor('#4A4848'),
                     border: Border.all(color: HexColor('#EAAD13'))),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Login",
+                      style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        fontSize: 30,
+                        color: HexColor('#EBAF76'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10.0, 30, 10, 10),
+                      child: RaisedButton(
+                        color: HexColor('#B3B1B1'),
+                        child: Container(
+                          height: 50,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Container(
+                                  height: 25,
+                                  width: 25,
+                                  child: Image.asset("images/google.png"),
+                                ),
+                              ),
+                              Text(
+                                "Sign in with Google",
+                                style: TextStyle(
+                                    color: HexColor('#333333'), fontSize: 18),
+                              ),
+                            ],
+                          ),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        onPressed: () {
+                          null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 40,
+                  width: 100,
+                  child: Image.asset('images/designwhite.png'),
+                ),
               ),
             ],
           ),
